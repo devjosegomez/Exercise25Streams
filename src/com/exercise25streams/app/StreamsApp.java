ade1f79dfafd7b6fc6b3e1de24bfa3727c64f04d
devjosegomez <devjosegomez@gmail.com>
2019-10-24 11:39:11 -0500

    Reading files .txt

--- src/com/exercise25streams/app/StreamsApp.java
+++ src/com/exercise25streams/app/StreamsApp.java
@@ -1,10 +1,36 @@
 package com.exercise25streams.app;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class StreamsApp {
 
 	public static void main(String[] args) {	
+		String line = "";
+		File myFile = null;
+		FileReader myReader = null;
+		BufferedReader myBuffer = null;
 		
-
+		try {
+			myFile = new File("C:\\a\\a.txt");
+			myReader = new FileReader(myFile);
+			myBuffer = new BufferedReader(myReader);
+			
+			line = myBuffer.readLine();
+			System.out.println(line);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				myBuffer.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			myReader = null;
+			myBuffer = null;
+		}
 	}
-
 }
